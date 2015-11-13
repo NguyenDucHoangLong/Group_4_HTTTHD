885e3a30bf6efe4fdc356e0c45b9b0201ddbab4d
1212205 <1212205@student.hcmus.edu.vn>
2015-11-13 19:24:56 +0700

    project

---
+++ Source/QLWebsite/QLWebsite/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLWebsite.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/Admin/
+        public ActionResult Index()
+        {
+            if (Session["user"] == null)
+                return RedirectToAction("Login", "Login");
+            else
+                return View();
+        }
+	}
+}
---
+++ Source/QLWebsite/QLWebsite/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLWebsite.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Admin/Login/
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult CheckLogin(string email,string password)
+        {
+            Session["user"] = email;
+            return RedirectToAction("Index","Admin");
+        }
+	}
+}
