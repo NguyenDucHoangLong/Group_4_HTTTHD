928e014319f810b8b2ac8926c4d7796df03d8ac8
Hoang Long <ndhlong1994@gmail.com>
2015-12-25 09:35:51 +0700

    commit .net long

--- Source/QLWebsite/QLWebsite/Areas/Manager/Controllers/QuanLySPKhuyenMaiController.cs
+++ Source/QLWebsite/QLWebsite/Areas/Manager/Controllers/QuanLySPKhuyenMaiController.cs
@@ -19,7 +19,7 @@ namespace QLWebsite.Areas.Manager.Controllers
         public ActionResult Index(int? Page)
         {
             int pageNumber = (Page ?? 1);
-            int pageSize = 5;
+            int pageSize = 10;
             return View(db.SanPhamKhuyenMais.ToList().OrderBy(n => n.MaKM).ToPagedList(pageNumber, pageSize));
         }
 
@@ -35,6 +35,8 @@ namespace QLWebsite.Areas.Manager.Controllers
         [HttpGet]
         public ActionResult ThemMoi()
         {
+            ViewBag.MaKM = new SelectList(db.CTKhuyenMais, "MaKM", "TenCTKM");
+            ViewBag.MaSP = new SelectList(db.SanPhams, "MaSP", "TenSP");
             return View();
         }
 
@@ -43,8 +45,91 @@ namespace QLWebsite.Areas.Manager.Controllers
         {
             SanPhamKhuyenMai spKhuyenMai = new SanPhamKhuyenMai();
             spKhuyenMai.MaKM = int.Parse(f["MaKM"]);
-   
-            return View();
+            spKhuyenMai.MaSP = int.Parse(f["MaSP"]);
+            SanPhamKhuyenMai is_spKhuyenMai;
+            is_spKhuyenMai = db.SanPhamKhuyenMais.Where(n => n.MaKM == spKhuyenMai.MaKM && n.MaSP == spKhuyenMai.MaSP).SingleOrDefault();
+            if(is_spKhuyenMai == null)
+            {
+                
+                spKhuyenMai.ChietKhau = int.Parse(f["ChietKhau"]);
+                db.SanPhamKhuyenMais.Add(spKhuyenMai);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("ThemMoi");
+            }
+
         }
+
+        //Chỉnh sửa
+        [HttpGet]
+        public ActionResult ChinhSua(int MaKM, int MaSP)
+        {
+            //Lấy ra đối tượng theo mã
+            SanPhamKhuyenMai spKhuyenMai = db.SanPhamKhuyenMais
+                .Where(n => n.MaKM == MaKM && n.MaSP == MaSP).SingleOrDefault();
+            if (spKhuyenMai == null )
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(spKhuyenMai);
+        }
+
+        [HttpPost]
+        public ActionResult ChinhSua(FormCollection f, int MaKM, int MaSP)
+        {
+            SanPhamKhuyenMai spKhuyenMai = db.SanPhamKhuyenMais
+                .Where(n => n.MaKM == MaKM && n.MaSP == MaSP).SingleOrDefault();
+            spKhuyenMai.ChietKhau = int.Parse(f["ChietKhau"]);
+
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //Xóa
+        [HttpGet]
+        public ActionResult Xoa(int MaKM, int MaSP)
+        {
+
+            //Lấy ra đối tượng theo mã
+            SanPhamKhuyenMai spKhuyenMai = db.SanPhamKhuyenMais
+                .Where(n => n.MaKM == MaKM && n.MaSP == MaSP).SingleOrDefault();
+            if (spKhuyenMai == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(spKhuyenMai);
+        }
+
+        [HttpPost, ActionName("Xoa")]
+        public ActionResult XacNhanXoa(int MaKM, int MaSP)
+        {
+            //Lấy ra đối tượng theo mã
+            SanPhamKhuyenMai spKhuyenMai = db.SanPhamKhuyenMais
+                .Where(n => n.MaKM == MaKM && n.MaSP == MaSP).SingleOrDefault();
+            if (spKhuyenMai == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            db.SanPhamKhuyenMais.Remove(spKhuyenMai);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult CheckName(string makm, string masp)
+        {
+            int MaKM = int.Parse(makm);
+            int MaSP = int.Parse(masp);
+            var respone = new { Code = "1", Mgs = "success" };
+            var result = db.SanPhamKhuyenMais.Where(s => s.MaKM == MaKM && s.MaSP == MaSP).FirstOrDefault();
+            if (result != null)
+                respone = new { Code = "0", Mgs = "fail" };
+            return Json(respone, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
---
+++ Source/QLWebsite/QLWebsite/Areas/Manager/Controllers/SanPhamKhuyenMaisController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QLWebsite.Models;
+
+namespace QLWebsite.Areas.Manager.Controllers
+{
+    public class SanPhamKhuyenMaisController : Controller
+    {
+        private QLWEBSITEEntities db = new QLWEBSITEEntities();
+
+        // GET: Manager/SanPhamKhuyenMais
+        public ActionResult Index()
+        {
+            var sanPhamKhuyenMais = db.SanPhamKhuyenMais.Include(s => s.CTKhuyenMai).Include(s => s.SanPham);
+            return View(sanPhamKhuyenMais.ToList());
+        }
+
+        // GET: Manager/SanPhamKhuyenMais/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SanPhamKhuyenMai sanPhamKhuyenMai = db.SanPhamKhuyenMais.Find(id);
+            if (sanPhamKhuyenMai == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sanPhamKhuyenMai);
+        }
+
+        // GET: Manager/SanPhamKhuyenMais/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaKM = new SelectList(db.CTKhuyenMais, "MaKM", "TenCTKM");
+            ViewBag.MaSP = new SelectList(db.SanPhams, "MaSP", "TenSP");
+            return View();
+        }
+
+        // POST: Manager/SanPhamKhuyenMais/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaKM,MaSP,ChietKhau")] SanPhamKhuyenMai sanPhamKhuyenMai)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SanPhamKhuyenMais.Add(sanPhamKhuyenMai);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaKM = new SelectList(db.CTKhuyenMais, "MaKM", "TenCTKM", sanPhamKhuyenMai.MaKM);
+            ViewBag.MaSP = new SelectList(db.SanPhams, "MaSP", "TenSP", sanPhamKhuyenMai.MaSP);
+            return View(sanPhamKhuyenMai);
+        }
+
+        // GET: Manager/SanPhamKhuyenMais/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SanPhamKhuyenMai sanPhamKhuyenMai = db.SanPhamKhuyenMais.Find(id);
+            if (sanPhamKhuyenMai == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaKM = new SelectList(db.CTKhuyenMais, "MaKM", "TenCTKM", sanPhamKhuyenMai.MaKM);
+            ViewBag.MaSP = new SelectList(db.SanPhams, "MaSP", "TenSP", sanPhamKhuyenMai.MaSP);
+            return View(sanPhamKhuyenMai);
+        }
+
+        // POST: Manager/SanPhamKhuyenMais/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaKM,MaSP,ChietKhau")] SanPhamKhuyenMai sanPhamKhuyenMai)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sanPhamKhuyenMai).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MaKM = new SelectList(db.CTKhuyenMais, "MaKM", "TenCTKM", sanPhamKhuyenMai.MaKM);
+            ViewBag.MaSP = new SelectList(db.SanPhams, "MaSP", "TenSP", sanPhamKhuyenMai.MaSP);
+            return View(sanPhamKhuyenMai);
+        }
+
+        // GET: Manager/SanPhamKhuyenMais/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SanPhamKhuyenMai sanPhamKhuyenMai = db.SanPhamKhuyenMais.Find(id);
+            if (sanPhamKhuyenMai == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sanPhamKhuyenMai);
+        }
+
+        // POST: Manager/SanPhamKhuyenMais/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SanPhamKhuyenMai sanPhamKhuyenMai = db.SanPhamKhuyenMais.Find(id);
+            db.SanPhamKhuyenMais.Remove(sanPhamKhuyenMai);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
